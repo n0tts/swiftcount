094b3b105cbafee6074dc109a9e760fd36892e32
nott80 <swiftmediasolution@gmail.com>
2015-01-04 01:55:37 +0000

    Built navigation, added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
-Rails.application.routes.draw do
+Swiftcount::Application.routes.draw do
+  get "welcome/index"
+  get "welcome/about"
+  get "welcome/contact"
+  get "welcome/features"
+  get "welcome/faqs"
+  get "welcome/pricing"
   resources :accountings
 
+  root to: 'welcome#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
