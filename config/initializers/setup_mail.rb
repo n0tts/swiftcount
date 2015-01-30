6644d0ad88d59899ec7ee6f2ff8fa7c5fe2e76f6
nott80 <swiftmediasolution@gmail.com>
2015-01-30 22:42:55 +0000

    adding mailgun email services

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :address               =>  'smtp.sendgrid.net',
+  :address               =>  'smtp.mailgun.org',
   :port                  =>  '587',
   :authentication        =>  :plain,
-  :user_name             =>  'app32943444@heroku.com',
-  :password              =>  '3ylqd7cb',
-  :domain                =>  'heroku.com',
+  :user_name             =>  'postmaster@sandbox17fb0261e15b4894b544f8d4e05892dd.mailgun.org',
+  :password              =>  'd5ffb30d60b9c78921ddfd496e481b17',
+  :domain                =>  'sandbox17fb0261e15b4894b544f8d4e05892dd.mailgun.org',
   :enable_starttls_auto  =>  true
   }
